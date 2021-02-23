d33e006607cde33f1983eb7a48dbd5aca5fe6a0d
vivek-333.com <vivek-333.com>
2021-02-23 18:20:55 +0530

    changed

--- practice.py
+++ practice.py
@@ -479,6 +479,29 @@
 # z=''.join(c)
 # print(z)
 
+                ##============== CALCULATOR ===============
+
+# def calculator():
+#         if operator=='+':
+#                 c=a+b
+#                 return('addtion of {} + {} = {}'.format(a,b,c))
+#         elif operator=='-':
+#                 c=a-b
+#                 return('addtion of {} - {} = {}'.format(a,b,c))
+#         elif operator=='*':
+#                 c=a*b
+#                 return('multiplication of {} * {} = {}'.format(a,b,c))
+#         elif operator=='/':
+#                 c=a/b
+#                 return('division of {} / {} = {}'.format(a,b,c))
+
+# if __name__ == '__main__':
+#         a=int(input('first value : '))
+#         b=int(input('first value : '))
+#         operator=input()
+#         result=calculator()
+
+
 
 
 
